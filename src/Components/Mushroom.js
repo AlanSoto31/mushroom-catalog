546e093bbdec5386d9aa8f80a37cf6bb945c26a6
AlanSoto31 <soto.alan@hotmail.com>
2021-09-14 15:40:31 -0500

    Create Mushroom component

---
+++ src/Components/Mushroom.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+
+const Mushroom = ({ mushroom }) => (
+  <Col sm={12} md={6} lg={4} className="mb-4">
+    <Card className="h-100">
+      <Card.Img variant="top" className="mushroomList-card-img" src={mushroom.img_url} />
+      <Card.Title className="font-weight-bold text-center mt-4">
+        <Link className="card-title cardTitle" to={`/${mushroom.id}`}>
+          {mushroom.name}
+        </Link>
+        <p>{mushroom.scientific_name}</p>
+      </Card.Title>
+      <Card.Body className="d-flex flex-column justify-content-end ">
+        <Card.Text>
+          <span>
+            {mushroom.edibility}
+          </span>
+        </Card.Text>
+
+        <Card.Text>
+          <span>
+            {mushroom.description}
+          </span>
+        </Card.Text>
+
+      </Card.Body>
+    </Card>
+  </Col>
+);
+
+Mushroom.propTypes = {
+  mushroom: PropTypes.shape({
+    /* id: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+    ]), */
+    id: PropTypes.number,
+    name: PropTypes.string,
+    img_url: PropTypes.string,
+    edibility: PropTypes.string,
+    description: PropTypes.string,
+    scientific_name: PropTypes.string,
+  }).isRequired,
+};
+
+export default Mushroom;
--- src/Containers/MushroomList.js
+++ src/Containers/MushroomList.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Mushroom from '../Components/Mushroom';
 
-const MushroomList = () => (
-  <h1>MushroomList</h1>
-);
+const MushroomList = () => {
+  const mushroom = {
+    id: 1,
+    name: 'Horse',
+    img_url: 'https://www.first-nature.com/fungi/images/agaricaceae/agaricus-arvensis1.jpg',
+    description: 'Agaricus arvensis appears in manured meadows and beside bridle paths and other places where there is plenty of decaying organic matter, upon which it feeds saprophytically.',
+    scientific_name: 'Agaricus arvensis',
+    wiki_url: 'https://en.wikipedia.org/wiki/Agaricus_arvensis',
+    edibility: 'Edible and good',
+    dimensions: 'Cap 8-20 cm diameter, stem 8-10 cm tall * 2-3 cm diameter',
+    location: 'North America, Europe',
+  };
+
+  return (
+    <>
+      <Mushroom mushroom={mushroom} />
+    </>
+  );
+};
 
 export default MushroomList;
